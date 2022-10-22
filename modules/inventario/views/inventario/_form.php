2bc8319790df88bac007f50dd3f681f738055752
Josué Paiz <jpaiz@applaudostudios.dev>
2022-10-22 13:59:00 -0600

    Add styles inventory table

--- modules/inventario/views/inventario/_form.php
+++ modules/inventario/views/inventario/_form.php
@@ -1,25 +1,46 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+use kartik\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\TblProductos
 
 /** @var yii\web\View $this */
 /** @var app\models\TblInventario $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="tbl-inventario-form">
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'id_producto')->textInput() ?>
-
-    <?= $form->field($model, 'cantidad')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-header">
+            <h3 class="card-title">Crear / Editar Inventario</h3>
+        </div>
+        <?php $form = ActiveForm::begin(['type' => 
+        ActiveForm::TYPE_HORIZONTAL]); ?>
+        <div class="card-body">
+            <form role="form">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?= Html::activeLabel($model, 'id_producto', ['class' => 'control-label']) ?>
+                        <?= $form->field($model, 'id_producto', ['showLabels' => false])->widget(Select2::class, [
+                            'data' => ArrayHelper::map(TblProductos::find()->all(), 'id_producto', 'nombre'),
+                            'language' => 'es',
+                            'options' => ['placeholder' => '- Seleccionar Producto -'],
+                            'pluginOptions' => ['allowClear' => true],
+                        ]); ?>
+                    </div>
+                    <div class="col-md-12">
+                        <?= Html::activeLabel($model, 'cantidad', ['class' => 'control-label']) ?>
+                        <?= $form->field($model, 'cantidad', ['showLabels' => false])->textInput(['maxlength' => true]) ?>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <?= Html::submitButton($model->isNewRecord ? '<i class="fa fa-save"></i> Guardar' : '<i class="fa fa-save"></i> Actualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+                    <?= Html::a('<i class="fa fa-ban"></i> Cancelar', ['index'], ['class' => 'btn btn-danger']) ?>
+                </div>
+            </form>
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
--- modules/inventario/views/inventario/index.php
+++ modules/inventario/views/inventario/index.php
@@ -1,45 +1,128 @@
 <?php
 
 use app\models\TblInventario;
+use app\models\TblProductos;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\helpers\ArrayHelper;
+use kartik\export\ExportMenu;
 
 /** @var yii\web\View $this */
 /** @var app\modules\inventario\models\InventarioSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Tbl Inventarios';
+$this->title = 'Inventario';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="tbl-inventario-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row">
+    <div class="col-md-12">
+        <div class="tbl-inventario-index">
 
-    <p>
-        <?= Html::a('Create Tbl Inventario', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+            <h1><?= Html::encode($this->title) ?></h1>
+            <?php
+            $gridColumns = [
+                [
+                    'class' => 'kartik\grid\SerialColumn',
+                    'contentOptions' => ['class' => 'kartik-sheet-style'],
+                    'width' => '36px',
+                    'header' => '#',
+                    'headerOptions' => ['class' => 'kartik-sheet-style']
+                ],
+                [
+                    'class' => 'kartik\grid\DataColumn',
+                    'width' => '60px',
+                    'format' => 'raw',
+                    'vAlign' => 'middle',
+                    'hAlign' => 'center',
+                    'attribute' => 'id',
+                    'value' => function ($model, $key, $index, $widget) {
+                        return Html::tag('span', $model->id, ['class' => 'badge bg-purple']);
+                    },
+                    'filter' => false,
+                ],
+                
+                [
+                    'class' => 'kartik\grid\DataColumn',
+                    'attribute' => 'id_producto',
+                    'vAlign' => 'middle',
+                    'format' => 'html',
+                    'value' => function ($model, $key, $index, $widget) {
+                        return Html::a($model->producto->nombre,  ['productos/view', 'id_producto' => $model->producto->id_producto]);
+                    },
+                    'filterType' => GridView::FILTER_SELECT2,
+                    'filter' => ArrayHelper::map(TblProductos::find()->orderBy('nombre')->all(), 'id_producto', 'nombre'),
+                    'filterWidgetOptions' => [
+                        'options' => ['placeholder' => 'Todos...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ],
+                ],
+                [
+                    'class' => 'kartik\grid\ActionColumn',
+                    'urlCreator' => function ($action, TblInventario $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            ];
+            $exportmenu = ExportMenu::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => $gridColumns,
+                'exportConfig' => [
+                    ExportMenu::FORMAT_TEXT => false,
+                    ExportMenu::FORMAT_HTML => false,
+                    ExportMenu::FORMAT_CSV => false,
+                ],
+            ]);
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+            echo GridView::widget([
+                'id' => 'kv-pacientes',
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => $gridColumns,
+                'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
+                'headerRowOptions' => ['class' => 'kartik-sheet-style'],
+                'filterRowOptions' => ['class' => 'kartik-sheet-style'],
+                'pjax' => true, // pjax is set to always true for this demo
+                // set your toolbar
+                'toolbar' =>  [
+                    [
+                        'content' =>
+                        Html::a('<i class="fas fa-plus"></i> Agregar', ['create'], [
+                            'class' => 'btn btn-success',
+                            'data-pjax' => 0,
+                        ]) . ' ' .
+                            Html::a('<i class="fas fa-redo"></i>', ['index'], [
+                                'class' => 'btn btn-outline-success',
+                                'data-pjax' => 0,
+                            ]),
+                        'options' => ['class' => 'btn-group mr-2']
+                    ],
+                    '{toggleData}',
+                    $exportmenu,
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                ],
+                'toggleDataContainer' => ['class' => 'btn-group mr-2'],
+                // set export properties
+                // parameters from the demo form
+                'bordered' => true,
+                'striped' => true,
+                'condensed' => true,
+                'responsive' => true,
+                'hover' => true,
+                //'showPageSummary'=>$pageSummary,
+                'panel' => [
+                    'type' => GridView::TYPE_PRIMARY,
+                    'heading' => 'Pacientes',
+                ],
+                'persistResize' => false,
+            ]);
+            ?>
 
-            'id',
-            'id_producto',
-            'cantidad',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, TblInventario $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-
+        </div>
+    </div>
 </div>
+
